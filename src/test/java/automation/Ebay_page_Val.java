eb96163068de759d364f7bcd279a8a65a0e89ce0
Raihan <rayanmondo@gmail.com>
2022-01-11 23:15:57 -0500

    added code for max of an array

--- src/test/java/automation/Ebay_page_Val.java
+++ src/test/java/automation/Ebay_page_Val.java
@@ -1,11 +1,15 @@
 package automation;
 
+import java.util.Arrays;
+
 public class Ebay_page_Val {
     public static void main(String[] args) {
-        int[] n={1,2,3,4,5,6};
+        int[] n = {1, 2, 3, 4, 5, 6};
         System.out.println(n.length);
-        
-        for(int i=0;i=n.length;i++){
-            System.out.println(i);
+
+        for (int i = 0; i <=n.length; i++) {
+
+        }
+        System.out.println(Arrays.stream(n).max());
     }
 }
